bbd29dd6844888ddd8b23d1f556afcbda9179e78
TiagoViniciusDev <tiagoviniciusdev@gmail.com>
2025-05-06 07:40:35 -0300

    Verificação de URL

--- frontend/src/components/Main/Main.jsx
+++ frontend/src/components/Main/Main.jsx
@@ -1,7 +1,28 @@
 import './Main.css'
 import { FaLink } from "react-icons/fa";
 
+import { useState } from 'react';
+
 function Main() {
+
+  const [url, setUrl] = useState()
+  const [validURL, setValidURL] = useState(false)
+
+  // console.log(validURL)
+
+  function inputValue(URL){
+    setUrl(URL)
+    checkURL(URL)
+  }
+
+  function checkURL(URL){
+    if(URL.includes("https") && URL.includes("youtu")){
+      setValidURL(true)
+    } else{
+      setValidURL(false)
+    }
+  }
+
   return (
     <div className='Main'>
         <div className='mainContainer'>
@@ -10,9 +31,9 @@ function Main() {
             <form>
                 <div>
                     <FaLink />
-                    <input type="text" placeholder='Cole a url do vídeo do Youtube aqui'/>
+                    <input type="text" placeholder='Cole a url do vídeo do Youtube aqui' onChange={(e) => {inputValue(e.target.value)}}/>
                 </div>
-                <button type='submit'>Resumir</button>
+                <button type='submit' style={validURL ? {backgroundColor: "aqua"} : {}}>Resumir</button>
             </form>
         </div>
     </div>
